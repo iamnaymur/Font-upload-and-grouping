a0b88c8646140a1d0891715f3cdc8a57453d005b
iamnaymur <nrtusher1@gmail.com>
2023-08-31 08:16:36 +0600

    uploader

--- src/Components/uploaderForm/Form.jsx
+++ src/Components/uploaderForm/Form.jsx
@@ -1,14 +1,42 @@
-import React from "react";
-import { FaCloudUploadAlt } from 'react-icons/fa';
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 const Form = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const onDrop = (acceptedFiles) => {
+    const ttfFiles = acceptedFiles.filter((file) => file.name.endsWith(".ttf"));
+
+    if (ttfFiles.length === 0) {
+      setWarningMessage("Please select a valid TTF file.");
+    } else {
+      setSelectedFiles(ttfFiles);
+      setWarningMessage("");
+    }
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: ".ttf",
+  });
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="max-w-md p-8 bg-white rounded shadow-md">
-      <h1 className="flex items-center justify-center"><FaCloudUploadAlt/></h1>
-        <h1>Click to upload or drag and drop.</h1>
-        <h1 className="text-xs">Only TTF File Allowed.</h1>
-      </div>
+    <div className="h-screen flex items-center justify-center ">
+      <form
+        {...getRootProps()}
+        className="w-3/4 p-8 bg-white rounded shadow-black shadow-2xl"
+      >
+        <input {...getInputProps()} />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-7xl">
+            <FaCloudUploadAlt />
+          </h1>
+          <h1>Click to upload or drag and drop.</h1>
+          <h1 className="text-xs bold">Only TTF File Allowed.</h1>
+        </div>
+      </form>
     </div>
   );
 };
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,7 +4,6 @@ import Form from "../Components/uploaderForm/Form";
 const HomePage = () => {
   return (
     <div>
-      <h1>This is the homepage</h1>
       <Form />
     </div>
   );
